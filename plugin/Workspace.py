9b30f94daf6839771e8dca78f327d91e885d92d0
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-21 11:50:08 -0500

    update internal imports after lib upgrade

--- plugin/Workspace.py
+++ plugin/Workspace.py
@@ -1,24 +1,21 @@
-import os
-import traceback
 import zlib
 
-from nanome.util import Logs
-from nanome._internal._network._serialization import _ContextSerialization, _ContextDeserialization
-from nanome._internal._structure._serialization import _WorkspaceSerializer, _AtomSerializer
-from nanome._internal._util._serializers import _DictionarySerializer, _StringSerializer, _ByteSerializer, _TypeSerializer, _LongSerializer
+from nanome._internal.network.context import ContextSerialization, ContextDeserialization
+from nanome._internal.structure.serializers import _WorkspaceSerializer, _AtomSerializer
+from nanome._internal.util.type_serializers import DictionarySerializer, StringSerializer, ByteSerializer, TypeSerializer, LongSerializer
 
 # This package uses undocumented network code, in order to reuse already available serialization code
 
 workspace_serializer = _WorkspaceSerializer()
-dictionary_serializer = _DictionarySerializer()
-dictionary_serializer.set_types(_StringSerializer(), _ByteSerializer())
-atom_dictionary_serializer = _DictionarySerializer()
-atom_dictionary_serializer.set_types(_LongSerializer(), _AtomSerializer())
+dictionary_serializer = DictionarySerializer()
+dictionary_serializer.set_types(StringSerializer(), ByteSerializer())
+atom_dictionary_serializer = DictionarySerializer()
+atom_dictionary_serializer.set_types(LongSerializer(), _AtomSerializer())
 
 def to_data(workspace):
-    context = _ContextSerialization(0, _TypeSerializer.get_version_table())
+    context = ContextSerialization(0, TypeSerializer.get_version_table())
     context.write_uint(0) # Version
-    context.write_using_serializer(dictionary_serializer, _TypeSerializer.get_version_table())
+    context.write_using_serializer(dictionary_serializer, TypeSerializer.get_version_table())
 
     subcontext = context.create_sub_context()
     subcontext.payload["Atom"] = {}
@@ -30,12 +27,12 @@ def to_data(workspace):
 
 def from_data(data):
     data = zlib.decompress(data)
-    context = _ContextDeserialization(data, _TypeSerializer.get_version_table())
+    context = ContextDeserialization(data, TypeSerializer.get_version_table())
     context.read_uint()
     file_version_table = context.read_using_serializer(dictionary_serializer)
-    version_table = _TypeSerializer.get_best_version_table(file_version_table)
+    version_table = TypeSerializer.get_best_version_table(file_version_table)
 
-    context = _ContextDeserialization(data, version_table)
+    context = ContextDeserialization(data, version_table)
     context.read_uint()
     context.read_using_serializer(dictionary_serializer)
     context.payload["Atom"] = context.read_using_serializer(atom_dictionary_serializer)
