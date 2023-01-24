37556431e07be8d7b1ef6a79def3b078d4d2e843
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-01-24 11:13:21 -0500

    Try to remove writes to filesystem after container build

--- docker/build.sh
+++ docker/build.sh
@@ -21,4 +21,6 @@ fi
 
 cachebust=`cat .cachebust`
 docker build -f plugin.Dockerfile --build-arg CACHEBUST=$cachebust -t vault:latest ..
-docker build -f server.Dockerfile -t vault-server:latest ..
+# DO NOT MERGE INTO MASTER
+docker build -f server.Dockerfile -t public.ecr.aws/h7r1e4h2/vault-server:openshift ..
+docker push public.ecr.aws/h7r1e4h2/vault-server:openshift
--- docker/server.Dockerfile
+++ docker/server.Dockerfile
@@ -8,7 +8,9 @@ COPY server/yarn.lock server/package.json ./
 RUN yarn install --production
 COPY server .
 RUN cd ui && yarn install && yarn build
-
+RUN mkdir -p /app/Documents/nanome-vault
+RUN mkdir -p /app/Documents/shared
+RUN chmod +w -R /app/Documents
 EXPOSE 80 443
 
 CMD yarn start ${ARGS}
