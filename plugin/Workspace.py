a0c82a2395748e5a9d661fa86012db7d15fa0f82
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2023-02-20 22:50:41 -0500

    Fix imports changed in nanome-lib 0.39.3 (#64)

--- plugin/Workspace.py
+++ plugin/Workspace.py
@@ -1,20 +1,21 @@
 import zlib
 
+from nanome.api.structure.serializers import WorkspaceSerializer, AtomSerializer
 from nanome._internal.network.context import ContextSerialization, ContextDeserialization
-from nanome._internal.structure.serializers import _WorkspaceSerializer, _AtomSerializer
-from nanome._internal.util.type_serializers import DictionarySerializer, StringSerializer, ByteSerializer, TypeSerializer, LongSerializer
+from nanome._internal.serializer_fields import DictionaryField, StringField, ByteField, TypeSerializer, LongField
 
 # This package uses undocumented network code, in order to reuse already available serialization code
 
-workspace_serializer = _WorkspaceSerializer()
-dictionary_serializer = DictionarySerializer()
-dictionary_serializer.set_types(StringSerializer(), ByteSerializer())
-atom_dictionary_serializer = DictionarySerializer()
-atom_dictionary_serializer.set_types(LongSerializer(), _AtomSerializer())
+workspace_serializer = WorkspaceSerializer()
+dictionary_serializer = DictionaryField()
+dictionary_serializer.set_types(StringField(), ByteField())
+atom_dictionary_serializer = DictionaryField()
+atom_dictionary_serializer.set_types(LongField(), AtomSerializer())
+
 
 def to_data(workspace):
     context = ContextSerialization(0, TypeSerializer.get_version_table())
-    context.write_uint(0) # Version
+    context.write_uint(0)  # Version
     context.write_using_serializer(dictionary_serializer, TypeSerializer.get_version_table())
 
     subcontext = context.create_sub_context()
@@ -24,6 +25,7 @@ def to_data(workspace):
     context.write_bytes(subcontext.to_array())
 
     return zlib.compress(context.to_array())
+
 
 def from_data(data):
     data = zlib.decompress(data)
